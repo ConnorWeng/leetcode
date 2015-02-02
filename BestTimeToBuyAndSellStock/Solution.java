fae51e084b8fd916426e6b1a1fb1cd440a79695b
Connor Weng <foamtea30@gmail.com>
2015-02-02 16:57:30 +0800

    modify Best Time to Buy and Sell Stock

--- BestTimeToBuyAndSellStock/Solution.java
+++ BestTimeToBuyAndSellStock/Solution.java
@@ -2,7 +2,9 @@
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
  *
- * Runtime: 424 ms (too slow!!)
+ * Runtime: 218 ms (too slow!!)
+ *
+ * 思路：迭代prices数组，过程中记录下最低的price，然后每一次遇到拐点（前一天高后一天低，或者最后一天）的时候，就拿这个price和最低的price计算一个profit，如果profit比之前的大，就替换之前的，最后得出结果。
  */
 public class Solution {
     int lastLeastPos = 0;
@@ -10,30 +12,21 @@ public class Solution {
     public int maxProfit(int[] prices) {
         if (prices.length < 2) return 0;
         int maxProfit = 0;
-        int max = prices[0];
+        int last = prices[0];
+        int min = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > max && i != prices.length - 1) {
-                max = prices[i];
-            } else {
+            if (prices[i] < last || i == prices.length - 1) {
                 int profit = 0;
-                if (prices[i] > max && i == prices.length - 1) {
-                    profit = profitIfLastPosIs(i, prices);
+                if (prices[i] > last && i == prices.length - 1) {
+                    profit = prices[i] - min;
                 } else {
-                    profit = profitIfLastPosIs(i - 1, prices);
+                    profit = prices[i - 1] - min;
                 }
                 if (profit > maxProfit) maxProfit = profit;
-                max = prices[i];
             }
+            last = prices[i];
+            if (prices[i] < min) min = prices[i];
         }
         return maxProfit;
-    }
-
-    public int profitIfLastPosIs(int pos, int[] prices) {
-        for (int i = lastLeastPos; i < pos; i++) {
-            if (prices[i] < prices[lastLeastPos]) {
-                lastLeastPos = i;
-            }
-        }
-        return prices[pos] - prices[lastLeastPos];
     }
 }
